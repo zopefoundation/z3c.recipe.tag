d524e8c9edf4633ee06323790132436d44cd8124
Marius Gedminas <marius@gedmin.as>
2012-01-11 16:39:36 +0000

    Back to development: 0.4.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name="z3c.recipe.tag",
-    version='0.4.1',
+    version='0.4.2dev',
     author="Ignas Mikalajūnas and the Zope Community",
     description="Generate ctags from eggs for development.",
     long_description=(read('README.txt')
