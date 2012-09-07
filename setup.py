3568ca4e747ff64d15d1010cfb5e737cd4c98bb0
Christian Theune <ct@gocept.com>
2012-09-07 10:02:18 +0000

    Preparing release 0.6

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name="z3c.recipe.tag",
-    version='0.6.dev0',
+    version='0.6',
     author="Ignas Mikalajūnas and the Zope Community",
     description="Generate ctags from eggs for development.",
     long_description=(read('README.txt')
