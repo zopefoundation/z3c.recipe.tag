a5406e7984816e1afc35c7a215071de0a84fed4f
Marius Gedminas <marius@gedmin.as>
2014-10-20 08:36:23 +0300

    Make the tests pass with tox

--- src/z3c/recipe/tag/tests.py
+++ src/z3c/recipe/tag/tests.py
@@ -90,6 +90,8 @@ if os.getenv('RUNNING_UNDER_TOX'):
          ''),
         (re.compile("\s*'/sample-buildout/eggs/distribute-pyN.N.egg',\n"),
          ''),
+        (re.compile("\s*'/sample-buildout/eggs/setuptools-pyN.N.egg',\n"),
+         ''),
     ])
 
 
